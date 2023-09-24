ccb2898aab21c7e4a496eeb2de64a45304f7a8e9
Yasuhito Takamiya <yasuhito@hey.com>
2023-09-24 16:41:37 +0900

    Update x-gate.ts

--- x-gate.ts
+++ x-gate.ts
@@ -2,6 +2,7 @@ import * as PIXI from "pixi.js";
 import { Gate } from "./gate";
 
 export class XGate extends Gate {
+  // FIXME: XGate ではなく Mixin (SquareShape, CircularShape) で cornerRadius を設定する
   static cornerRadius = 9999;
   static icon = PIXI.Texture.from("./assets/X.svg");
 }
