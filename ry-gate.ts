865a5551d211df658425f77c423e9362e54712ba
Yasuhito Takamiya <yasuhito@hey.com>
2023-09-26 07:56:27 +0900

    Ry ゲートを追加

---
+++ ry-gate.ts
@@ -0,0 +1,7 @@
+import * as PIXI from "pixi.js";
+import { Gate } from "./gate";
+import { SquareGateMixin } from "./square-gate-mixin";
+
+export class RyGate extends SquareGateMixin(Gate) {
+  static icon = PIXI.Texture.from("./assets/Ry.svg");
+}
