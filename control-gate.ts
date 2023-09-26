430b1a208a5bafcad53c576897f1acf8da5fa9fb
Yasuhito Takamiya <yasuhito@hey.com>
2023-09-26 15:14:20 +0900

    コントロールゲートを追加 (#15)

---
+++ control-gate.ts
@@ -0,0 +1,106 @@
+import * as PIXI from "pixi.js";
+import { Gate } from "./gate";
+import * as tailwindColors from "tailwindcss/colors";
+
+export class ControlGate extends Gate {
+  static icon = PIXI.Texture.from("./assets/Control.svg");
+  static iconHover = PIXI.Texture.from("./assets/Control_hover.svg");
+  static iconGrabbed = PIXI.Texture.from("./assets/Control_grabbed.svg");
+  static iconActive = PIXI.Texture.from("./assets/Control_active.svg");
+
+  static style = {
+    idleBodyColor: null,
+    idleBorderColor: null,
+
+    hoverBodyColor: null,
+    hoverBorderColor: tailwindColors.purple["500"],
+    hoverBorderWidth: 2,
+
+    grabbedBodyColor: tailwindColors.purple["500"],
+    grabbedBorderColor: tailwindColors.purple["700"],
+    grabbedBorderWidth: 1,
+
+    activeBodyColor: null,
+    activeBorderColor: tailwindColors.teal["300"],
+    activeBorderWidth: 2,
+
+    cornerRadius: 4,
+  };
+
+  get style(): typeof ControlGate.style {
+    return ControlGate.style;
+  }
+
+  applyIdleStyle() {
+    this.sprite.texture = ControlGate.icon;
+
+    this.graphics.clear();
+    this.graphics.zIndex = 0;
+    this.graphics.cursor = "default";
+
+    this.updateGraphics(this.style.idleBodyColor, this.style.idleBorderColor);
+  }
+
+  applyHoverStyle() {
+    this.sprite.texture = ControlGate.iconHover;
+
+    this.graphics.clear();
+    this.graphics.zIndex = 0;
+    this.graphics.cursor = "grab";
+
+    this.updateGraphics(
+      this.style.hoverBodyColor,
+      this.style.hoverBorderColor,
+      this.style.hoverBorderWidth
+    );
+  }
+
+  applyGrabbedStyle() {
+    this.sprite.texture = ControlGate.iconGrabbed;
+
+    this.graphics.clear();
+    this.graphics.zIndex = 10;
+    this.graphics.cursor = "grabbing";
+
+    this.updateGraphics(
+      this.style.grabbedBodyColor,
+      this.style.grabbedBorderColor,
+      this.style.grabbedBorderWidth
+    );
+  }
+
+  applyActiveStyle() {
+    this.sprite.texture = ControlGate.iconActive;
+
+    this.graphics.clear();
+    this.graphics.zIndex = 0;
+    this.graphics.cursor = "grab";
+
+    this.updateGraphics(
+      this.style.activeBodyColor,
+      this.style.activeBorderColor,
+      this.style.activeBorderWidth
+    );
+  }
+
+  private updateGraphics(
+    bodyColor: string | null,
+    borderColor: string | null,
+    borderWidth: number | null = null
+  ) {
+    if (borderWidth !== null && borderColor !== null) {
+      this.graphics.lineStyle(borderWidth, borderColor, 1, 0);
+    }
+    if (bodyColor !== null) {
+      this.graphics.beginFill(bodyColor, 1);
+    }
+    this.graphics.drawRoundedRect(
+      0,
+      0,
+      Gate.size,
+      Gate.size,
+      this.style.cornerRadius
+    );
+    this.graphics.endFill();
+  }
+}
