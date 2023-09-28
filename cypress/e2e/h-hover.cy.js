6766b1f87ce942e949edcdc7fbd5ab1520d45c1a
Yasuhito Takamiya <yasuhito@hey.com>
2023-09-28 09:52:59 +0900

    Update h-hover.cy.js

--- cypress/e2e/h-hover.cy.js
+++ cypress/e2e/h-hover.cy.js
@@ -4,7 +4,7 @@ describe("hover H gate", () => {
   it("passes", () => {
     cy.visit("/");
     // cy.get("#app").trigger("mousemove", { clientX: 174, clientY: 48 });
-    cy.get("#app").trigger("mousemove", 174, 48);
+    cy.get("#app").trigger("mousemove", 174 + 16, 48 + 16);
     cy.percySnapshot();
   });
 });
