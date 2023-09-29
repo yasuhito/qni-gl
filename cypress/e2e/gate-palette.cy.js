3588e10b074097593944b2488958d22f0241f2a2
Yasuhito Takamiya <yasuhito@hey.com>
2023-09-29 14:00:23 +0900

    テストを追加

--- cypress/e2e/gate-palette.cy.js
+++ cypress/e2e/gate-palette.cy.js
@@ -1,40 +1,51 @@
 import { HGate } from "../../h-gate";
 import { XGate } from "../../x-gate";
+import { YGate } from "../../y-gate";
 
 describe("Gate Palette", () => {
+  let gatePaletteComponents;
+
   before(() => {
-    cy.visit("/");
+    cy.visit("/").then(() => {
+      cy.get("#app").then(($appEl) => {
+        gatePaletteComponents = $appEl.data().components.gatePalette.gates;
+      });
+    });
   });
 
   describe("H Gate", () => {
     it("changes style when mouseover", () => {
-      cy.get("#app").then(($appEl) => {
-        const hGatePosition = $appEl.data().components.gatePalette.gates.HGate;
-
-        cy.get("canvas")
-          .realMouseMove(
-            hGatePosition.x + HGate.size / 2,
-            hGatePosition.y + HGate.size / 2
-          )
-          .percySnapshot("Hover over the H gate");
-      });
+      cy.visit("/")
+        .get("canvas")
+        .realMouseMove(
+          gatePaletteComponents.HGate.x + HGate.size / 2,
+          gatePaletteComponents.HGate.y + HGate.size / 2
+        )
+        .percySnapshot("Hover over the H gate");
     });
   });
 
   describe("X Gate", () => {
     it("changes style when mouseover", () => {
-      cy.visit("/");
+      cy.visit("/")
+        .get("canvas")
+        .realMouseMove(
+          gatePaletteComponents.XGate.x + XGate.size / 2,
+          gatePaletteComponents.XGate.y + XGate.size / 2
+        )
+        .percySnapshot("Hover over the X gate");
+    });
+  });
 
-      cy.get("#app").then(($appEl) => {
-        const xGatePosition = $appEl.data().components.gatePalette.gates.XGate;
-
-        cy.get("canvas")
-          .realMouseMove(
-            xGatePosition.x + XGate.size / 2,
-            xGatePosition.y + XGate.size / 2
-          )
-          .percySnapshot("Hover over the X gate");
-      });
+  describe("Y Gate", () => {
+    it("changes style when mouseover", () => {
+      cy.visit("/")
+        .get("canvas")
+        .realMouseMove(
+          gatePaletteComponents.YGate.x + YGate.size / 2,
+          gatePaletteComponents.YGate.y + YGate.size / 2
+        )
+        .percySnapshot("Hover over the Y gate");
     });
   });
 });
