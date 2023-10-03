37351db7298a1af07aedc6fcb66ac03c9c5db293
Yasuhito Takamiya <yasuhito@hey.com>
2023-10-03 15:20:08 +0900

    テストを追加

--- tests/gate-palette.spec.ts
+++ tests/gate-palette.spec.ts
@@ -148,4 +148,14 @@ test.describe("Gate Palette", () => {
       await expect(page).toHaveScreenshot("rz-gate-hover.png");
     });
   });
+
+  test.describe("Swap Gate", () => {
+    test("changes style when mouseover", async ({ page }) => {
+      const gate = gatePalette.gates.SwapGate;
+
+      await page.mouse.move(gate.x + gate.width / 2, gate.y + gate.height / 2);
+
+      await expect(page).toHaveScreenshot("swap-gate-hover.png");
+    });
+  });
 });
