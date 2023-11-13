5f4ba398acbb4f663fcf91ae11e42c44bb86cb20
Yasuhito Takamiya <yasuhito@hey.com>
2023-11-13 17:00:00 +0900

    四角ゲートの色を修正

--- src/square-gate-mixin.ts
+++ src/square-gate-mixin.ts
@@ -1,12 +1,9 @@
-import * as tailwindColors from "tailwindcss/colors";
 import { Colors } from "./colors";
 import { Constructor } from "./constructor";
 import { Gate } from "./gate";
-import { Graphics } from "pixi.js";
+import { Spacing } from "./spacing";
 
 export declare class SquareGate {
-  static style: { [key: string]: number | string };
-  style: { [key: string]: number | string };
   applyIdleStyle(): void;
   applyHoverStyle(): void;
   applyGrabbedStyle(): void;
@@ -17,87 +14,94 @@ export function SquareGateMixin<TBase extends Constructor<Gate>>(
   Base: TBase
 ): Constructor<SquareGate> & TBase {
   class SquareGateMixinClass extends Base {
-    static style = {
-      idleBodyColor: Colors.bg.brand.default,
-      idleBorderColor: Colors.border.gate.idle,
-      idleBorderWidth: 2,
-      hoverBodyColor: Colors.bg.brand.hover,
-      hoverBorderColor: tailwindColors.purple["500"],
-      hoverBorderWidth: 2,
-      grabbedBodyColor: Colors.bg.brand.grabbed,
-      grabbedBorderColor: tailwindColors.purple["700"],
-      grabbedBorderWidth: 2,
-      activeBodyColor: Colors.bg.brand.active,
-      activeBorderColor: tailwindColors.teal["300"],
-      activeBorderWidth: 2,
-      cornerRadius: 4,
-    };
-
-    get style(): typeof SquareGateMixinClass.style {
-      return SquareGateMixinClass.style;
-    }
-
     applyIdleStyle() {
       this._shape.clear();
+
       this._shape.zIndex = 0;
       this._shape.cursor = "default";
 
-      this.updateGraphics(
-        this.style.idleBodyColor,
-        this.style.idleBorderColor,
-        this.style.idleBorderWidth
+      this._shape.lineStyle(
+        Spacing.borderWidth.gate,
+        Colors.border.gate.idle,
+        1,
+        0
       );
-    }
-
-    applyHoverStyle() {
-      this._shape.clear();
-      this._shape.zIndex = 0;
-      this._shape.cursor = "grab";
-
-      this.updateGraphics(
-        this.style.hoverBodyColor,
-        this.style.hoverBorderColor,
-        this.style.hoverBorderWidth
-      );
-    }
-
-    applyGrabbedStyle() {
-      this._shape.clear();
-      this._shape.zIndex = 10;
-      this._shape.cursor = "grabbing";
-
-      this.updateGraphics(
-        this.style.grabbedBodyColor,
-        this.style.grabbedBorderColor,
-        this.style.grabbedBorderWidth
-      );
-    }
-
-    applyActiveStyle() {
-      this._shape.clear();
-      this._shape.zIndex = 0;
-      this._shape.cursor = "grab";
-
-      this.updateGraphics(
-        this.style.activeBodyColor,
-        this.style.activeBorderColor,
-        this.style.activeBorderWidth
-      );
-    }
-
-    private updateGraphics(
-      bodyColor: string,
-      borderColor: string,
-      borderWidth: number
-    ) {
-      this._shape.lineStyle(borderWidth, borderColor, 1, 0);
-      this._shape.beginFill(bodyColor, 1);
+      this._shape.beginFill(Colors.bg.brand.default, 1);
       this._shape.drawRoundedRect(
         0,
         0,
         Gate.size,
         Gate.size,
-        this.style.cornerRadius
+        Spacing.cornerRadius.gate
+      );
+      this._shape.endFill();
+    }
+
+    applyHoverStyle() {
+      this._shape.clear();
+
+      this._shape.zIndex = 0;
+      this._shape.cursor = "grab";
+
+      this._shape.lineStyle(
+        Spacing.borderWidth.gate,
+        Colors.border.gate.hover,
+        1,
+        0
+      );
+      this._shape.beginFill(Colors.bg.brand.hover, 1);
+      this._shape.drawRoundedRect(
+        0,
+        0,
+        Gate.size,
+        Gate.size,
+        Spacing.cornerRadius.gate
+      );
+      this._shape.endFill();
+    }
+
+    applyGrabbedStyle() {
+      this._shape.clear();
+
+      this._shape.zIndex = 10;
+      this._shape.cursor = "grabbing";
+
+      this._shape.lineStyle(
+        Spacing.borderWidth.gate,
+        Colors.border.gate.grabbed,
+        1,
+        0
+      );
+      this._shape.beginFill(Colors.bg.brand.grabbed, 1);
+      this._shape.drawRoundedRect(
+        0,
+        0,
+        Gate.size,
+        Gate.size,
+        Spacing.cornerRadius.gate
+      );
+      this._shape.endFill();
+    }
+
+    applyActiveStyle() {
+      this._shape.clear();
+
+      this._shape.zIndex = 0;
+      this._shape.cursor = "grab";
+
+      this._shape.lineStyle(
+        Spacing.borderWidth.gate,
+        Colors.border.gate.active,
+        1,
+        0
+      );
+      this._shape.beginFill(Colors.bg.brand.active, 1);
+      this._shape.drawRoundedRect(
+        0,
+        0,
+        Gate.size,
+        Gate.size,
+        Spacing.cornerRadius.gate
       );
       this._shape.endFill();
     }
