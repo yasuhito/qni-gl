d1d24017e0e366bce156509c46132140828eb058
Yasuhito Takamiya <yasuhito@hey.com>
2023-11-14 15:12:52 +0900

    Square gate design token (#33)

--- src/colors.ts
+++ src/colors.ts
@@ -3,20 +3,24 @@ import * as tailwindColors from "tailwindcss/colors";
 export const Colors = {
   bg: {
     brand: {
+      default: tailwindColors.sky["500"],
+      hover: tailwindColors.sky["600"],
       grabbed: tailwindColors.purple["500"],
-    }
+      active: tailwindColors.sky["500"],
+    },
   },
   border: {
     gate: {
+      idle: tailwindColors.sky["700"],
       hover: tailwindColors.purple["500"],
       grabbed: tailwindColors.purple["600"],
-      active: tailwindColors.teal["300"]
-    }
+      active: tailwindColors.teal["300"],
+    },
   },
   icon: {
     gate: {
       default: tailwindColors.white,
-      secondary: tailwindColors.sky["500"]
-    }
-  }
-}
+      secondary: tailwindColors.sky["500"],
+    },
+  },
+};
