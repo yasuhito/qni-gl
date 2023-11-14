bb603bdebc8098f0bc30ded953406d47c2ff9b35
Yasuhito Takamiya <yasuhito@hey.com>
2023-11-14 16:53:11 +0900

    vitest を修正

--- test/h-gate.test.ts
+++ test/h-gate.test.ts
@@ -2,23 +2,9 @@
 
 import { describe, expect, test } from "vitest";
 import { HGate } from "../src/h-gate";
-import * as tailwindColors from "tailwindcss/colors";
 
 describe("HGate", () => {
-  test("style", () => {
-    const hGate = new HGate();
-
-    expect(hGate.style.idleBodyColor).toBe(tailwindColors.emerald["500"]);
-    expect(hGate.style.idleBorderColor).toBe(tailwindColors.emerald["700"]);
-    expect(hGate.style.idleBorderWidth).toBe(1);
-    expect(hGate.style.hoverBodyColor).toBe(tailwindColors.emerald["500"]);
-    expect(hGate.style.hoverBorderColor).toBe(tailwindColors.purple["500"]);
-    expect(hGate.style.hoverBorderWidth).toBe(2);
-    expect(hGate.style.grabbedBodyColor).toBe(tailwindColors.purple["500"]);
-    expect(hGate.style.grabbedBorderColor).toBe(tailwindColors.purple["700"]);
-    expect(hGate.style.grabbedBorderWidth).toBe(1);
-    expect(hGate.style.activeBodyColor).toBe(tailwindColors.emerald["500"]);
-    expect(hGate.style.activeBorderColor).toBe(tailwindColors.teal["300"]);
-    expect(hGate.style.activeBorderWidth).toBe(2);
+  test("gateType", () => {
+    expect(HGate.gateType).toBe("HGate");
   });
 });
