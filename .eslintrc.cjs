7238e33c34121fa8b682e0b13139b3179b2b663f
Yasuhito Takamiya <yasuhito@hey.com>
2023-12-06 15:35:34 +0900

    lint スクリプトを追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        sourceType: "script",
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {},
+};
--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -1,21 +1,25 @@
 import { Simulator } from "@qni/simulator";
 
 // Install SW
-self.addEventListener("install", (event) => {
+self.addEventListener("install", () => {
   console.log("ServiceWorker installed");
 });
 
 // TODO: Qni の runSimulator にあたるハンドラを実行
 self.addEventListener("message", (event) => {
   const qubitCount = event.data.qubitCount;
-  const simulator = new Simulator('0'.repeat(qubitCount));
-  const vector = simulator.state.matrix.clone()
-  const amplitudes = []
+  const simulator = new Simulator("0".repeat(qubitCount));
+  const vector = simulator.state.matrix.clone();
+  const amplitudes = [];
 
   for (let i = 0; i < vector.height; i++) {
-    const c = vector.cell(0, i)
-    amplitudes.push([c.real, c.imag])
+    const c = vector.cell(0, i);
+    amplitudes.push([c.real, c.imag]);
   }
 
-  self.postMessage({ type: "finished", qubitCount: qubitCount, amplitudes: amplitudes });
+  self.postMessage({
+    type: "finished",
+    qubitCount: qubitCount,
+    amplitudes: amplitudes,
+  });
 });
