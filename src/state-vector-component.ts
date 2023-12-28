e979d063868e46778cf91f1c9d25a5971bd9fd8a
Yasuhito Takamiya <yasuhito@hey.com>
2023-12-28 15:50:46 +0900

    6qubit 対応 (#71)

--- src/state-vector-component.ts
+++ src/state-vector-component.ts
@@ -26,6 +26,8 @@ export class StateVectorComponent extends Container {
       this.qubitCirclesGridContainer.cols = 8;
     } else if (this.qubitCount == 5) {
       this.qubitCirclesGridContainer.cols = 8;
+    } else if (this.qubitCount == 6) {
+      this.qubitCirclesGridContainer.cols = 16;
     }
 
     this.clear();
