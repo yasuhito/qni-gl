0cd4d3b8a06184b6f5e39a14936c89dc2b6ce1b9
Yasuhito Takamiya <yasuhito@hey.com>
2024-03-05 09:48:31 +0900

    テストを修正

--- tests/playwright/dropzone.spec.ts
+++ tests/playwright/dropzone.spec.ts
@@ -20,7 +20,7 @@ test.describe("Dropzone", () => {
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(4000)
+    await page.waitForTimeout(5000)
 
     await expect(page).toHaveScreenshot("drag-and-drop-h-gate.png");
   });
@@ -32,7 +32,7 @@ test.describe("Dropzone", () => {
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(4000)
+    await page.waitForTimeout(5000)
 
     await expect(page).toHaveScreenshot("drag-and-drop-x-gate.png");
   });
@@ -44,7 +44,7 @@ test.describe("Dropzone", () => {
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(4000)
+    await page.waitForTimeout(5000)
 
     await expect(page).toHaveScreenshot("drag-and-drop-y-gate.png");
   });
@@ -56,7 +56,7 @@ test.describe("Dropzone", () => {
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(4000)
+    await page.waitForTimeout(5000)
 
     await expect(page).toHaveScreenshot("drag-and-drop-z-gate.png");
   });
@@ -68,7 +68,7 @@ test.describe("Dropzone", () => {
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(4000)
+    await page.waitForTimeout(5000)
 
     await expect(page).toHaveScreenshot("drag-and-drop-rnot-gate.png");
   });
