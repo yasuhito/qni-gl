4a701af15cb1d0fa3127f470e6f4fc266f2caccd
Yasuhito Takamiya <yasuhito@hey.com>
2024-04-10 14:18:26 +0900

    テストを修正

--- tests/playwright/dropzone.spec.ts
+++ tests/playwright/dropzone.spec.ts
@@ -1,9 +1,13 @@
+import { DropzoneComponent } from "../../src/dropzone-component";
+import { GatePaletteComponent } from "../../src/gate-palette-component";
 import { appData, centerPosition } from "./test-helpers";
-import { test, expect } from "@playwright/test";
+import { test, expect, Locator } from "@playwright/test";
 
 test.describe("Dropzone", () => {
-  let gatePalette;
-  let firstDropzone;
+  let gatePalette: GatePaletteComponent;
+  let firstDropzone: DropzoneComponent;
+  let running: Locator;
+  let idle: Locator;
 
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
@@ -11,16 +15,22 @@ test.describe("Dropzone", () => {
     const app = await appData(page);
     gatePalette = app.gatePalette;
     firstDropzone = app.circuit.steps[0].dropzones[0];
+    running = page.locator('#app[data-state="running"]');
+    idle = page.locator('#app[data-state="idle"]');
   });
 
   test("Drag and drop H gate", async ({ page }) => {
     const gate = gatePalette.gates.HGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(10000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-h-gate.png");
   });
@@ -28,11 +38,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop X gate", async ({ page }) => {
     const gate = gatePalette.gates.XGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(10000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-x-gate.png");
   });
@@ -40,11 +54,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop Y gate", async ({ page }) => {
     const gate = gatePalette.gates.YGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(10000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-y-gate.png");
   });
@@ -52,11 +70,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop Z gate", async ({ page }) => {
     const gate = gatePalette.gates.ZGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-z-gate.png");
   });
@@ -64,14 +86,14 @@ test.describe("Dropzone", () => {
   test("Drag and drop √X gate", async ({ page }) => {
     const gate = gatePalette.gates.RnotGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
 
-    const running = page.locator('#app[data-state="running"]');
     await running.waitFor();
-    const idle = page.locator('#app[data-state="idle"]');
     await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-rnot-gate.png");
@@ -80,11 +102,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop S gate", async ({ page }) => {
     const gate = gatePalette.gates.SGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-s-gate.png");
   });
@@ -92,11 +118,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop S† gate", async ({ page }) => {
     const gate = gatePalette.gates.SDaggerGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-s-dagger-gate.png");
   });
@@ -104,11 +134,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop T gate", async ({ page }) => {
     const gate = gatePalette.gates.TGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-t-gate.png");
   });
@@ -116,11 +150,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop T† gate", async ({ page }) => {
     const gate = gatePalette.gates.TDaggerGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-t-dagger-gate.png");
   });
@@ -128,11 +166,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop Swap gate", async ({ page }) => {
     const gate = gatePalette.gates.SwapGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-swap-gate.png");
   });
@@ -140,34 +182,31 @@ test.describe("Dropzone", () => {
   test("Drag and drop Control gate", async ({ page }) => {
     const gate = gatePalette.gates.ControlGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-control-gate.png");
   });
 
-  // test("Drag and drop Anti Control gate", async ({ page }) => {
-  //   const gate = gatePalette.gates.AntiControlGate;
-
-  //   await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
-  //   await page.mouse.down();
-  //   await page.mouse.move(firstDropzone.x, firstDropzone.y);
-  //   await page.mouse.up();
-
-  //   await expect(page).toHaveScreenshot("drag-and-drop-anti-control-gate.png");
-  // });
-
   test("Drag and drop |0> gate", async ({ page }) => {
     const gate = gatePalette.gates.Write0Gate;
+
+    await idle.waitFor();
 
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-write0-gate.png");
   });
@@ -175,11 +214,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop |1> gate", async ({ page }) => {
     const gate = gatePalette.gates.Write1Gate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
-    await page.waitForTimeout(5000);
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-write1-gate.png");
   });
@@ -187,10 +230,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop Measurement gate", async ({ page }) => {
     const gate = gatePalette.gates.MeasurementGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-measurement-gate.png");
   });
@@ -198,10 +246,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop Bloch Sphere", async ({ page }) => {
     const gate = gatePalette.gates.BlochSphere;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-bloch-sphere.png");
   });
@@ -209,10 +262,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop QFT gate", async ({ page }) => {
     const gate = gatePalette.gates.QFTGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-qft-gate.png");
   });
@@ -220,10 +278,15 @@ test.describe("Dropzone", () => {
   test("Drag and drop QFT† gate", async ({ page }) => {
     const gate = gatePalette.gates.QFTDaggerGate;
 
+    await idle.waitFor();
+
     await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
     await page.mouse.down();
     await page.mouse.move(firstDropzone.x, firstDropzone.y);
     await page.mouse.up();
+
+    await running.waitFor();
+    await idle.waitFor();
 
     await expect(page).toHaveScreenshot("drag-and-drop-qft-dagger-gate.png");
   });
