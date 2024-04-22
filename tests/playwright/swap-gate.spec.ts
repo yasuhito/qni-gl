f4ec4c89457f5f32f4c0d3140f4e4c85d8f5766b
Yasuhito Takamiya <yasuhito@hey.com>
2024-04-22 16:31:52 +0900

    Swap ゲートのシステムテストを追加

--- tests/playwright/swap-gate.spec.ts
+++ tests/playwright/swap-gate.spec.ts
@@ -1,22 +1,62 @@
 import { App } from "../../src/app";
+import { GateComponent } from "../../src";
 import { appData, centerPosition } from "./test-helpers";
 import { test, expect, Locator } from "@playwright/test";
 
 test.describe("Swap gate", () => {
   let app: App;
   let idle: Locator;
+  let swapGate: GateComponent | null;
 
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
 
     app = await appData(page);
     idle = page.locator('#app[data-state="idle"]');
+    swapGate = app.gatePalette.gates.SwapGate;
 
     await idle.waitFor();
   });
 
+  test("Place a Swap gate on the first bit", async ({ page }) => {
+    const dropzone = app.circuit.steps[0].dropzones[0];
+
+    await page.mouse.move(centerPosition(swapGate).x, centerPosition(swapGate).y);
+    await page.mouse.down();
+    await page.mouse.move(dropzone.x, dropzone.y);
+    await page.mouse.up();
+    await idle.waitFor();
+
+    await expect(page).toHaveScreenshot("swap-gate-bit1.png");
+  });
+
+  test("Place a Swap gate on the second bit", async ({ page }) => {
+    const dropzone = app.circuit.steps[0].dropzones[1];
+
+    await page.mouse.move(centerPosition(swapGate).x, centerPosition(swapGate).y);
+    await page.mouse.down();
+    await page.mouse.move(dropzone.x, dropzone.y);
+    await page.mouse.up();
+    await idle.waitFor();
+
+    await expect(page).toHaveScreenshot("swap-gate-bit2.png");
+  });
+
+  test("Place a Swap gate on the third bit", async ({ page }) => {
+    await page.mouse.move(centerPosition(swapGate).x, centerPosition(swapGate).y);
+    await page.mouse.down();
+
+    app = await appData(page);
+    const dropzone = app.circuit.steps[0].dropzones[2];
+
+    await page.mouse.move(dropzone.x, dropzone.y);
+    await page.mouse.up();
+    await idle.waitFor();
+
+    await expect(page).toHaveScreenshot("swap-gate-bit3.png");
+  });
+
   test("Two swap gates join", async ({ page }) => {
-    const swapGate = app.gatePalette.gates.SwapGate;
     const dropzoneBit0 = app.circuit.steps[0].dropzones[0];
     const dropzoneBit1 = app.circuit.steps[0].dropzones[1];
 
@@ -36,7 +76,6 @@ test.describe("Swap gate", () => {
   });
 
   test("Three swap gates don't join", async ({ page }) => {
-    const swapGate = app.gatePalette.gates.SwapGate;
     const dropzoneBit0 = app.circuit.steps[0].dropzones[0];
     const dropzoneBit1 = app.circuit.steps[0].dropzones[1];
 
