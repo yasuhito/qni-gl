3e467f32c5ea376ceff3433ac594e0dc658a177d
Yasuhito Takamiya <yasuhito@hey.com>
2024-04-22 10:22:18 +0900

    Z ゲートのシステムテストを追加 (#87)

---
+++ tests/playwright/z-gate.spec.ts
@@ -0,0 +1,58 @@
+import { App } from "../../src/app";
+import { GateComponent } from "../../src";
+import { appData, centerPosition } from "./test-helpers";
+import { test, expect, Locator } from "@playwright/test";
+
+test.describe("Z gate", () => {
+  let app: App;
+  let idle: Locator;
+  let zGate: GateComponent | null;
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+
+    app = await appData(page);
+    idle = page.locator('#app[data-state="idle"]');
+    zGate = app.gatePalette.gates.ZGate;
+
+    await idle.waitFor();
+  });
+
+  test("Place an Z gate on the first bit", async ({ page }) => {
+    const dropzone = app.circuit.steps[0].dropzones[0];
+
+    await page.mouse.move(centerPosition(zGate).x, centerPosition(zGate).y);
+    await page.mouse.down();
+    await page.mouse.move(dropzone.x, dropzone.y);
+    await page.mouse.up();
+    await idle.waitFor();
+
+    await expect(page).toHaveScreenshot("z-gate-bit1.png");
+  });
+
+  test("Place an Z gate on the second bit", async ({ page }) => {
+    const dropzone = app.circuit.steps[0].dropzones[1];
+
+    await page.mouse.move(centerPosition(zGate).x, centerPosition(zGate).y);
+    await page.mouse.down();
+    await page.mouse.move(dropzone.x, dropzone.y);
+    await page.mouse.up();
+    await idle.waitFor();
+
+    await expect(page).toHaveScreenshot("z-gate-bit2.png");
+  });
+
+  test("Place an Z gate on the third bit", async ({ page }) => {
+    await page.mouse.move(centerPosition(zGate).x, centerPosition(zGate).y);
+    await page.mouse.down();
+
+    app = await appData(page);
+    const dropzone = app.circuit.steps[0].dropzones[2];
+
+    await page.mouse.move(dropzone.x, dropzone.y);
+    await page.mouse.up();
+    await idle.waitFor();
+
+    await expect(page).toHaveScreenshot("z-gate-bit3.png");
+  });
+});
