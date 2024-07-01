af627f197eb741ae63177da76c34d37ff342777f
Yasuhito Takamiya <yasuhito@hey.com>
2024-07-01 13:12:18 +0900

    Dockerfile を追加

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,13 +4,14 @@
 set -e
 
 # Railsサーバーの起動
-cd /qni-gl/backend_rails
-bundle install
-./bin/rails s -d -b 0.0.0.0
+# cd /qni-gl/backend_rails
+# bundle install
+# ./bin/rails s -d -b 0.0.0.0
 
 # vite ディレクトリに移動
 cd /qni-gl/vite
 yarn
+yarn build
 
 # 任意のコマンドを実行
 exec "$@"
