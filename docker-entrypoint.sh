9f29ab7ab3b6e2292dccd7e526ef766c69c4772a
Yasuhito Takamiya <yasuhito@hey.com>
2024-07-03 10:48:06 +0900

    playwright のテストを動作させる

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,5 +11,8 @@ yarn build
 # nginx の起動
 nginx
 
+# uWSGI の起動
+uwsgi --chdir /qni-gl/backend --ini /qni-gl/backend/backend.ini --uid uwsgiuser --gid uwsgiuser --master --daemonize /var/log/uwsgi.log
+
 # 任意のコマンドを実行
 exec "$@"
