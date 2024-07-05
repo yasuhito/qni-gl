1b755814eed48668a09b1f2c6fa4964e888063c9
Yasuhito Takamiya <yasuhito@hey.com>
2024-07-05 11:33:21 +0900

    Docker 関係のリファクタリング (#104)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# エラーが発生した場合にスクリプトを終了する
+# Exit the script if any command fails
 set -e
 
-# vite のビルド
+# Build the Vite project
 cd /qni-gl/vite
-yarn
 yarn build
 
-# nginx の起動
-nginx
-
-# gunicorn の起動
+# Start gunicorn
 cd /qni-gl/backend
 gunicorn --bind unix:/tmp/gunicorn.sock --daemon
 
-# 任意のコマンドを実行
+# Start nginx
+nginx
+
+# Execute any passed command
 exec "$@"
