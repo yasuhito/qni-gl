68621313bc608cdd1702e9e6028fa142648daf03
Yasuhito Takamiya <yasuhito@hey.com>
2024-08-11 17:02:51 +0900

    テストのリファクタリング

--- frontend/tests/playwright/discard-gate.spec.ts
+++ frontend/tests/playwright/discard-gate.spec.ts
@@ -1,28 +1,18 @@
-import { GateComponent } from "../../src";
-import { GatePaletteComponent } from "../../src/gate-palette-component";
-import { appData, centerPosition } from "./test-helpers";
-import { test, expect, Locator } from "@playwright/test";
+import { expect, test } from "./fixtures";
 
 test.describe("Discard gates", () => {
-  let gatePalette: GatePaletteComponent;
-  let idle: Locator;
-
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
-
-    const app = await appData(page);
-    gatePalette = app.gatePalette;
-    idle = page.locator('#app[data-state="idle"]');
   });
 
-  test("Discard H gate", async ({ page }) => {
-    const gate = gatePalette.gates.HGate as GateComponent;
+  test("Discard gate", async ({ page, circuitInfo, idle }) => {
+    const gate = circuitInfo.gatePalette.hGate;
 
-    await page.mouse.move(centerPosition(gate).x, centerPosition(gate).y);
+    await page.mouse.move(gate.x, gate.y);
     await page.mouse.down();
     await page.mouse.up();
     await idle.waitFor();
 
-    await expect(page).toHaveScreenshot("discard-h-gate.png");
+    await expect(page).toHaveScreenshot("discard-gate.png");
   });
 });
