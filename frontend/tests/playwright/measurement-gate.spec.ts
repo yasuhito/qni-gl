10b41f2771fceaec43b0b2d056c764f6750b6d47
Yasuhito Takamiya <yasuhito@hey.com>
2024-08-12 14:36:39 +0900

    テストを修正

--- frontend/tests/playwright/measurement-gate.spec.ts
+++ frontend/tests/playwright/measurement-gate.spec.ts
@@ -1,62 +1,46 @@
-import { App } from "../../src/app";
-import { GateComponent } from "../../src";
-import { appData, centerPosition } from "./test-helpers";
-import { test, expect, Locator } from "@playwright/test";
+import { expect, test } from "./fixtures";
+import { dragAndDrop, getCircuitInfo } from "./test-helpers";
 
 test.describe("Measurement gate", () => {
-  let app: App;
-  let idle: Locator;
-  let measurementGate: GateComponent;
-
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
-
-    app = await appData(page);
-    idle = page.locator('#app[data-state="idle"]');
-    measurementGate = app.gatePalette.gates.MeasurementGate as GateComponent;
-
-    await idle.waitFor();
   });
 
-  test("Place a Measurement gate on the first bit", async ({ page }) => {
-    const dropzone = app.circuit.steps[0].dropzones[0];
+  test("Place a Measurement gate on the first bit", async ({
+    page,
+    circuitInfo,
+  }) => {
+    const measurementGate = circuitInfo.gatePalette.measurementGate;
+    const dropzone = circuitInfo.steps[0][0];
 
-    await page.mouse.move(
-      centerPosition(measurementGate).x,
-      centerPosition(measurementGate).y
-    );
-    await page.mouse.down();
-    await page.mouse.move(dropzone.x, dropzone.y);
-    await page.mouse.up();
-    await idle.waitFor();
+    await dragAndDrop(page, measurementGate, dropzone);
 
     await expect(page).toHaveScreenshot("measurement-gate-bit1.png");
   });
 
-  test("Place a Measurement gate on the second bit", async ({ page }) => {
-    const dropzone = app.circuit.steps[0].dropzones[1];
+  test("Place a Measurement gate on the second bit", async ({
+    page,
+    circuitInfo,
+  }) => {
+    const measurementGate = circuitInfo.gatePalette.measurementGate;
+    const dropzone = circuitInfo.steps[0][1];
 
-    await page.mouse.move(
-      centerPosition(measurementGate).x,
-      centerPosition(measurementGate).y
-    );
-    await page.mouse.down();
-    await page.mouse.move(dropzone.x, dropzone.y);
-    await page.mouse.up();
-    await idle.waitFor();
+    await dragAndDrop(page, measurementGate, dropzone);
 
     await expect(page).toHaveScreenshot("measurement-gate-bit2.png");
   });
 
-  test("Place a Measurement gate on the third bit", async ({ page }) => {
-    await page.mouse.move(
-      centerPosition(measurementGate).x,
-      centerPosition(measurementGate).y
-    );
+  test("Place a Measurement gate on the third bit", async ({
+    page,
+    circuitInfo,
+    idle,
+  }) => {
+    const measurementGate = circuitInfo.gatePalette.measurementGate;
+    await page.mouse.move(measurementGate.x, measurementGate.y);
     await page.mouse.down();
 
-    app = await appData(page);
-    const dropzone = app.circuit.steps[0].dropzones[2];
+    circuitInfo = await getCircuitInfo(page);
+    const dropzone = circuitInfo.steps[0][2];
 
     await page.mouse.move(dropzone.x, dropzone.y);
     await page.mouse.up();
