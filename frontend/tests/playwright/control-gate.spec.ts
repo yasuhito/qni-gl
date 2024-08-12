ce0fb8ce87c3f207730310b5d7f25e93cdaae412
Yasuhito Takamiya <yasuhito@hey.com>
2024-08-12 11:50:13 +0900

    テストを修正

--- frontend/tests/playwright/control-gate.spec.ts
+++ frontend/tests/playwright/control-gate.spec.ts
@@ -1,62 +1,46 @@
-import { App } from "../../src/app";
-import { GateComponent } from "../../src";
-import { appData, centerPosition } from "./test-helpers";
-import { test, expect, Locator } from "@playwright/test";
+import { expect, test } from "./fixtures";
+import { dragAndDrop, getCircuitInfo } from "./test-helpers";
 
 test.describe("Control gate", () => {
-  let app: App;
-  let idle: Locator;
-  let controlGate: GateComponent;
-
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
-
-    app = await appData(page);
-    idle = page.locator('#app[data-state="idle"]');
-    controlGate = app.gatePalette.gates.ControlGate as GateComponent;
-
-    await idle.waitFor();
   });
 
-  test("Place a Control gate on the first bit", async ({ page }) => {
-    const dropzone = app.circuit.steps[0].dropzones[0];
+  test("Place a Control gate on the first bit", async ({
+    page,
+    circuitInfo,
+  }) => {
+    const controlGate = circuitInfo.gatePalette.controlGate;
+    const dropzone = circuitInfo.steps[0][0];
 
-    await page.mouse.move(
-      centerPosition(controlGate).x,
-      centerPosition(controlGate).y
-    );
-    await page.mouse.down();
-    await page.mouse.move(dropzone.x, dropzone.y);
-    await page.mouse.up();
-    await idle.waitFor();
+    await dragAndDrop(page, controlGate, dropzone);
 
     await expect(page).toHaveScreenshot("control-gate-bit1.png");
   });
 
-  test("Place a Control gate on the second bit", async ({ page }) => {
-    const dropzone = app.circuit.steps[0].dropzones[1];
+  test("Place a Control gate on the second bit", async ({
+    page,
+    circuitInfo,
+  }) => {
+    const controlGate = circuitInfo.gatePalette.controlGate;
+    const dropzone = circuitInfo.steps[0][1];
 
-    await page.mouse.move(
-      centerPosition(controlGate).x,
-      centerPosition(controlGate).y
-    );
-    await page.mouse.down();
-    await page.mouse.move(dropzone.x, dropzone.y);
-    await page.mouse.up();
-    await idle.waitFor();
+    await dragAndDrop(page, controlGate, dropzone);
 
     await expect(page).toHaveScreenshot("control-gate-bit2.png");
   });
 
-  test("Place a Control gate on the third bit", async ({ page }) => {
-    await page.mouse.move(
-      centerPosition(controlGate).x,
-      centerPosition(controlGate).y
-    );
+  test("Place a Control gate on the third bit", async ({
+    page,
+    circuitInfo,
+    idle,
+  }) => {
+    const controlGate = circuitInfo.gatePalette.controlGate;
+    await page.mouse.move(controlGate.x, controlGate.y);
     await page.mouse.down();
 
-    app = await appData(page);
-    const dropzone = app.circuit.steps[0].dropzones[2];
+    circuitInfo = await getCircuitInfo(page);
+    const dropzone = circuitInfo.steps[0][2];
 
     await page.mouse.move(dropzone.x, dropzone.y);
     await page.mouse.up();
