f8ad6ef6281db443ed420e8c480e6b7ceaae3949
Yasuhito Takamiya <yasuhito@hey.com>
2024-08-31 11:42:42 +0900

    Prevent X gates from connecting unintentionally (#140)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vitest/config";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
   build: {
@@ -21,20 +20,13 @@ export default defineConfig({
   plugins: [
     VitePWA({
       strategies: "injectManifest",
+      registerType: "autoUpdate",
+      injectRegister: "auto",
       srcDir: "src",
       filename: "serviceWorker.ts",
-      injectRegister: "auto",
       injectManifest: {
         injectionPoint: undefined,
       },
-    }),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "assets",
-          dest: "",
-        },
-      ],
     }),
   ],
   test: {
