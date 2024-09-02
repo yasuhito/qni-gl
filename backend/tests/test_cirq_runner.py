f92a23155fc83be18ba66d9881448ef5411a1a80
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-03 08:30:00 +0900

    テストを追加

--- backend/tests/test_cirq_runner.py
+++ backend/tests/test_cirq_runner.py
@@ -425,6 +425,31 @@ class TestCirqRunner(unittest.TestCase):
         assert_complex_approx(amplitudes[0], 0, 0)
         assert_complex_approx(amplitudes[1], 0, 1)
 
+    # S†|0⟩=|0⟩
+    def test_s_dagger_0ket(self):
+        steps = [[{"type": "S†", "targets": [0]}]]
+        circuit, measurement_moment = self.cirq_runner.build_circuit(1, steps)
+
+        result = self.cirq_runner.run_circuit_until_step_index(
+            circuit, measurement_moment, 0, steps, [0, 1])
+
+        amplitudes = result[0][":amplitude"]
+        assert_complex_approx(amplitudes[0], 1, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+
+    # S†|1⟩=-i|1⟩
+    def test_s_dagger_1ket(self):
+        steps = [[{"type": "X", "targets": [0]}],
+                 [{"type": "S†", "targets": [0]}]]
+        circuit, measurement_moment = self.cirq_runner.build_circuit(1, steps)
+
+        result = self.cirq_runner.run_circuit_until_step_index(
+            circuit, measurement_moment, 1, steps, [0, 1])
+
+        amplitudes = result[1][":amplitude"]
+        assert_complex_approx(amplitudes[0], 0, 0)
+        assert_complex_approx(amplitudes[1], 0, -1)
+
 
 if __name__ == "__main__":
     unittest.main()
