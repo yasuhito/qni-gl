c486ca38e604437161946db89b1a6651a8f9e51c
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-02 10:41:20 +0900

    Update test_cirq_runner.py

--- backend/tests/test_cirq_runner.py
+++ backend/tests/test_cirq_runner.py
@@ -11,6 +11,18 @@ class TestCirqRunner(unittest.TestCase):
     def test_initialization(self):
         assert self.cirq_runner is not None  # unittestスタイルのassertを通常のassertに変更
 
+    def test_build_circuit(self):
+        qubit_count = 1
+        circuit_qni = [
+            [{'type': 'H', 'targets': [0]}],
+        ]
+
+        circuit, measurement_moment = self.cirq_runner.build_circuit(
+            qubit_count, circuit_qni)
+
+        assert len(circuit.all_qubits()) == 1
+        assert str(circuit[0].operations[0].gate) == 'H'
+
     # 他のテストメソッドを追加することができます
     # 例: build_circuit, run_circuit_until_step_index など
 
