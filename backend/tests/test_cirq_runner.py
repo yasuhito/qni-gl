4d65d265af5b0ef10e4ad875d7ae4532301f2411
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-03 17:38:06 +0900

    |0> のテストを分離

--- backend/tests/test_cirq_runner.py
+++ backend/tests/test_cirq_runner.py
@@ -35,16 +35,6 @@ class TestCirqRunner(unittest.TestCase):
         assert len(circuit.all_qubits()) == 3
         assert str(circuit[0].operations[0]) == "CCX(q(0), q(1), q(2))"
 
-    def test_build_circuit_with_write0_gate(self):
-        step = [
-            [{"type": "|0>", "targets": [0]}],
-        ]
-
-        circuit, _ = self.cirq_runner.build_circuit(step)
-
-        assert len(circuit.all_qubits()) == 1
-        assert str(circuit[0].operations[0]) == "reset(q(0))"
-
     def test_build_circuit_with_write1_gate(self):
         step = [
             [{"type": "|1>", "targets": [0]}],
---
+++ backend/tests/test_write0.py
@@ -0,0 +1,49 @@
+import unittest
+
+from src.cirq_runner import CirqRunner
+from tests.conftest import assert_complex_approx
+
+
+class TestWrite0(unittest.TestCase):
+    def setUp(self):
+        self.logger = None
+        self.cirq_runner = CirqRunner(self.logger)
+
+    def test_build_circuit(self):
+        step = [
+            [{"type": "|0>", "targets": [0]}],
+        ]
+
+        circuit, _ = self.cirq_runner.build_circuit(step)
+
+        assert len(circuit.all_qubits()) == 1
+        assert str(circuit[0].operations[0]) == "reset(q(0))"
+
+    # Write0|0⟩=|0>
+    def test_write0_0(self):
+        steps = [
+            [{"type": "|0>", "targets": [0]}],
+        ]
+        circuit, measurement_moment = self.cirq_runner.build_circuit(steps)
+
+        result = self.cirq_runner.run_circuit(
+            circuit, steps, measurement_moment)
+
+        amplitudes = result[0][":amplitude"]
+        assert_complex_approx(amplitudes[0], 1, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+
+    # Write0|1⟩=|0>
+    def test_write0_1(self):
+        steps = [
+            [{"type": "X", "targets": [0]}],
+            [{"type": "|0>", "targets": [0]}],
+        ]
+        circuit, measurement_moment = self.cirq_runner.build_circuit(steps)
+
+        result = self.cirq_runner.run_circuit(
+            circuit, steps, measurement_moment)
+
+        amplitudes = result[1][":amplitude"]
+        assert_complex_approx(amplitudes[0], 1, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
