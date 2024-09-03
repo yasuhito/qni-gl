fded8863c7bbbac1702e8220292956ec7cffc379
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-03 12:35:41 +0900

    カスタムアサーションを改善

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 
 
 def assert_complex_approx(actual, expected_real, expected_imag):
+    __tracebackhide__ = True
     assert pytest.approx(actual.real) == expected_real, f"Real parts differ: {
         actual.real} != {expected_real}"
     assert pytest.approx(actual.imag) == expected_imag, f"Imaginary parts differ: {
