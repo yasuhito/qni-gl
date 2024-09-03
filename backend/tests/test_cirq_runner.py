bcfdde8a5895f70d587778c7bf0272069327fe20
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-03 11:49:20 +0900

    Z ゲートのテストを分離

--- backend/tests/test_cirq_runner.py
+++ backend/tests/test_cirq_runner.py
@@ -37,17 +37,6 @@ class TestCirqRunner(unittest.TestCase):
         assert len(circuit.all_qubits()) == 3
         assert str(circuit[0].operations[0]) == "CCX(q(0), q(1), q(2))"
 
-    def test_build_circuit_with_z_gate(self):
-        qubit_count = 1
-        step = [
-            [{"type": "Z", "targets": [0]}],
-        ]
-
-        circuit, _ = self.cirq_runner.build_circuit(qubit_count, step)
-
-        assert len(circuit.all_qubits()) == 1
-        assert str(circuit[0].operations[0]) == "Z(q(0))"
-
     def test_build_circuit_with_rnot_gate(self):
         qubit_count = 1
         step = [
@@ -259,32 +248,6 @@ class TestCirqRunner(unittest.TestCase):
         amplitudes = result[0][":amplitude"]
         assert_complex_approx(amplitudes[0], 1 / sqrt(2), 0)
         assert_complex_approx(amplitudes[1], 1 / sqrt(2), 0)
-
-    # Z|0⟩=|0⟩
-
-    def test_z_0ket(self):
-        steps = [[{"type": "Z", "targets": [0]}]]
-        circuit, measurement_moment = self.cirq_runner.build_circuit(1, steps)
-
-        result = self.cirq_runner.run_circuit_until_step_index(
-            circuit, measurement_moment, 0, steps, [0, 1])
-
-        amplitudes = result[0][":amplitude"]
-        assert_complex_approx(amplitudes[0], 1, 0)
-        assert_complex_approx(amplitudes[1], 0, 0)
-
-    # Z|1⟩=-|1⟩
-    def test_z_1ket(self):
-        steps = [[{"type": "X", "targets": [0]}],
-                 [{"type": "Z", "targets": [0]}]]
-        circuit, measurement_moment = self.cirq_runner.build_circuit(1, steps)
-
-        result = self.cirq_runner.run_circuit_until_step_index(
-            circuit, measurement_moment, 1, steps, [0, 1])
-
-        amplitudes = result[1][":amplitude"]
-        assert_complex_approx(amplitudes[0], 0, 0)
-        assert_complex_approx(amplitudes[1], -1, 0)
 
     # X^½|0⟩=1/2((1+i)|0⟩+(1-i)|1⟩)
     def test_rnot_0ket(self):
---
+++ backend/tests/test_z.py
@@ -0,0 +1,46 @@
+import unittest
+
+from src.cirq_runner import CirqRunner
+from tests.conftest import assert_complex_approx
+
+
+class TestZ(unittest.TestCase):
+    def setUp(self):
+        self.logger = None
+        self.cirq_runner = CirqRunner(self.logger)
+
+    def test_build_circuit_with_z_gate(self):
+        qubit_count = 1
+        step = [
+            [{"type": "Z", "targets": [0]}],
+        ]
+
+        circuit, _ = self.cirq_runner.build_circuit(qubit_count, step)
+
+        assert len(circuit.all_qubits()) == 1
+        assert str(circuit[0].operations[0]) == "Z(q(0))"
+
+    # Z|0⟩=|0⟩
+    def test_z_0ket(self):
+        steps = [[{"type": "Z", "targets": [0]}]]
+        circuit, measurement_moment = self.cirq_runner.build_circuit(1, steps)
+
+        result = self.cirq_runner.run_circuit(
+            circuit, steps, measurement_moment)
+
+        amplitudes = result[0][":amplitude"]
+        assert_complex_approx(amplitudes[0], 1, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+
+    # Z|1⟩=-|1⟩
+    def test_z_1ket(self):
+        steps = [[{"type": "X", "targets": [0]}],
+                 [{"type": "Z", "targets": [0]}]]
+        circuit, measurement_moment = self.cirq_runner.build_circuit(1, steps)
+
+        result = self.cirq_runner.run_circuit(
+            circuit, steps, measurement_moment)
+
+        amplitudes = result[1][":amplitude"]
+        assert_complex_approx(amplitudes[0], 0, 0)
+        assert_complex_approx(amplitudes[1], -1, 0)
