0378ffc129d9cc935bbeba9ea92a287644105922
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-03 11:30:13 +0900

    H ゲートのテストを分離

--- backend/tests/test_h.py
+++ backend/tests/test_h.py
@@ -21,28 +21,32 @@ class TestH(unittest.TestCase):
         assert len(circuit.all_qubits()) == 1
         assert str(circuit[0].operations[0]) == "H(q(0))"
 
-    def test_h_0ket(self):
+    def test_h_0(self):
         qubit_count = 1
         steps = [
             [{"type": "H", "targets": [0]}],
         ]
-        circuit, measurement_moment = self.cirq_runner.build_circuit(qubit_count, steps)
+        circuit, measurement_moment = self.cirq_runner.build_circuit(
+            qubit_count, steps)
 
-        result = self.cirq_runner.run_circuit_until_step_index(circuit, measurement_moment, 0, steps, [0, 1])
+        result = self.cirq_runner.run_circuit_until_step_index(
+            circuit, measurement_moment, 0, steps, [0, 1])
 
         amplitudes = result[0][":amplitude"]
         assert_complex_approx(amplitudes[0], 1 / sqrt(2), 0)
         assert_complex_approx(amplitudes[1], 1 / sqrt(2), 0)
 
-    def test_h_1ket(self):
+    def test_h_1(self):
         qubit_count = 1
         steps = [
             [{"type": "X", "targets": [0]}],
             [{"type": "H", "targets": [0]}],
         ]
-        circuit, measurement_moment = self.cirq_runner.build_circuit(qubit_count, steps)
+        circuit, measurement_moment = self.cirq_runner.build_circuit(
+            qubit_count, steps)
 
-        result = self.cirq_runner.run_circuit_until_step_index(circuit, measurement_moment, 1, steps, [0, 1])
+        result = self.cirq_runner.run_circuit_until_step_index(
+            circuit, measurement_moment, 1, steps, [0, 1])
 
         amplitudes = result[1][":amplitude"]
         assert_complex_approx(amplitudes[0], 1 / sqrt(2), 0)
