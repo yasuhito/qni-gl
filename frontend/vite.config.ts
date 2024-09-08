34fc5f1c5dd0aac32025e864225a8e10baf1be7d
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-08 10:18:22 +0900

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,58 +1,53 @@
 import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from "vitest/config";
-import { loadEnv } from "vite";
 
-export default defineConfig(({ mode }) => {
-  loadEnv(mode, process.cwd());
-
-  return {
-    build: {
-      rollupOptions: {
-        output: {
-          manualChunks(id) {
-            if (id.includes("node_modules")) {
-              return id
-                .toString()
-                .split("node_modules/")[1]
-                .split("/")[0]
-                .toString();
-            }
-          },
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
         },
       },
     },
-    plugins: [
-      VitePWA({
-        srcDir: "src",
-        filename: "service-worker.js",
-        strategies: "injectManifest",
-        registerType: "autoUpdate",
-        injectRegister: false,
-        manifest: false,
-        injectManifest: {
-          injectionPoint: undefined,
-        },
-        devOptions: {
-          enabled: true,
-          type: "module",
-        },
-      }),
-    ],
-    test: {
-      coverage: {
-        provider: "istanbul",
-        reporter: ["json", "html"],
-        include: ["src/**"],
+  },
+  plugins: [
+    VitePWA({
+      srcDir: "src",
+      filename: "service-worker.js",
+      strategies: "injectManifest",
+      registerType: "autoUpdate",
+      injectRegister: false,
+      manifest: false,
+      injectManifest: {
+        injectionPoint: undefined,
       },
-      browser: {
-        provider: "playwright", // or 'webdriverio'
+      devOptions: {
         enabled: true,
-        name: "chromium", // browser name is required
-        headless: true,
+        type: "module",
       },
+    }),
+  ],
+  test: {
+    coverage: {
+      provider: "istanbul",
+      reporter: ["json", "html"],
+      include: ["src/**"],
     },
-    optimizeDeps: {
-      exclude: ["fsevents"],
+    browser: {
+      provider: "playwright", // or 'webdriverio'
+      enabled: true,
+      name: "chromium", // browser name is required
+      headless: true,
     },
-  };
+  },
+  optimizeDeps: {
+    exclude: ["fsevents"],
+  },
 });
