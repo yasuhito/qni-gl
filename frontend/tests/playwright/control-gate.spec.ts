a04662646759d8b2a1247f8e2aa8ecf3416d676b
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-16 15:24:22 +0900

    テストを修正

--- frontend/tests/playwright/control-gate.spec.ts
+++ frontend/tests/playwright/control-gate.spec.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "./fixtures";
-import { dragAndDrop, getCircuitInfo } from "./test-helpers";
+import { dragAndDrop } from "./test-helpers";
 
 test.describe("Control gate", () => {
   test.beforeEach(async ({ page }) => {
@@ -24,24 +24,17 @@ test.describe("Control gate", () => {
     const controlGate = circuitInfo.gatePalette.controlGate;
 
     await dragAndDrop(page, controlGate, { step: 0, bit: 1 });
+
     await expect(page).toHaveScreenshot("control-gate-bit2.png");
   });
 
   test("Place a Control gate on the third bit", async ({
     page,
     circuitInfo,
-    idle,
   }) => {
     const controlGate = circuitInfo.gatePalette.controlGate;
-    await page.mouse.move(controlGate.x, controlGate.y);
-    await page.mouse.down();
 
-    circuitInfo = await getCircuitInfo(page);
-    const dropzone = circuitInfo.steps[0][2];
-
-    await page.mouse.move(dropzone.x, dropzone.y);
-    await page.mouse.up();
-    await idle.waitFor();
+    await dragAndDrop(page, controlGate, { step: 0, bit: 2 });
 
     await expect(page).toHaveScreenshot("control-gate-bit3.png");
   });
