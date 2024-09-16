ac3f44a90f7dd02e92f549a9401592202397e595
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-16 13:16:04 +0900

    リファクタリング

--- frontend/src/h-gate.ts
+++ frontend/src/h-gate.ts
@@ -3,43 +3,18 @@ import { JsonableMixin } from "./jsonable-mixin";
 import { LabelableMixin } from "./labelable-mixin";
 import { SerializeableMixin } from "./serializeable-mixin";
 import { SquareGateMixin } from "./square-gate-mixin";
-import { SerializedGate } from "./types";
+import { ControllableMixin } from "./controllable-mixin";
 
 export class HGate extends SquareGateMixin(
-  SerializeableMixin(JsonableMixin(LabelableMixin(OperationComponent)))
+  ControllableMixin(
+    SerializeableMixin(JsonableMixin(LabelableMixin(OperationComponent)))
+  )
 ) {
-  private _controls: number[] = [];
-
   get operationType(): string {
     return "HGate";
   }
 
   get label(): string {
     return "H";
-  }
-
-  serialize(targetBits: number[], controlBits?: number[]): SerializedGate {
-    if (controlBits && controlBits.some((bit) => targetBits.includes(bit))) {
-      throw new Error("Overlap detected between target bits and control bits.");
-    }
-
-    const serialized: SerializedGate = {
-      type: this.serializeType,
-      targets: targetBits,
-    };
-
-    if (controlBits && controlBits.length > 0) {
-      serialized.controls = controlBits;
-    }
-
-    return serialized;
-  }
-
-  get controls() {
-    return this._controls;
-  }
-
-  set controls(value: number[]) {
-    this._controls = value.sort();
   }
 }
