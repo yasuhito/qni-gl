220a8db0e50bbcdc750034e48aa092950a8e36a3
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-17 12:53:22 +0900

    Update conftest.py

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,13 +1,13 @@
 import pytest
 
 
-def assert_complex_approx(actual, expected_real, expected_imag, abs_tol=1e-7):
+def assert_complex_approx(actual, expected_real, expected_imag):
     __tracebackhide__ = True
     assert pytest.approx(actual.real) == expected_real, f"Real parts differ: {actual.real} != {expected_real}"
     assert pytest.approx(actual.imag) == expected_imag, f"Imaginary parts differ: {actual.imag} != {expected_imag}"
 
 
-def assert_amplitudes_approx(actual_amplitudes, expected_amplitudes, abs_tol=1e-7):
+def assert_amplitudes_approx(actual_amplitudes, expected_amplitudes):
     __tracebackhide__ = True
     for key, (expected_real, expected_imag) in expected_amplitudes.items():
         actual_real, actual_imag = actual_amplitudes[key]
