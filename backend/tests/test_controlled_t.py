7c98fc4cd0b5f20047b951d815179d3c4212c742
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-18 10:51:16 +0900

    バックエンドに controlled-T を追加

---
+++ backend/tests/test_controlled_t.py
@@ -0,0 +1,113 @@
+import unittest
+from math import sqrt
+
+from src.qiskit_runner import QiskitRunner
+from tests.conftest import assert_complex_approx
+
+class TestControlledT(unittest.TestCase):
+    def setUp(self):
+        self.qiskit_runner = QiskitRunner()
+
+    def test_controlled_t_00(self):
+        steps = [[{"type": "T", "targets": [1], "controls": [0]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[0]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 1, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+        assert_complex_approx(amplitudes[2], 0, 0)
+        assert_complex_approx(amplitudes[3], 0, 0)
+
+    def test_controlled_t_01(self):
+        steps = [[{"type": "X", "targets": [0]}], [{"type": "T", "targets": [1], "controls": [0]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[1]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 0, 0)
+        assert_complex_approx(amplitudes[1], 1, 0)
+        assert_complex_approx(amplitudes[2], 0, 0)
+        assert_complex_approx(amplitudes[3], 0, 0)
+
+    def test_controlled_t_10(self):
+        steps = [[{"type": "X", "targets": [1]}], [{"type": "T", "targets": [1], "controls": [0]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[1]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 0, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+        assert_complex_approx(amplitudes[2], 1, 0)
+        assert_complex_approx(amplitudes[3], 0, 0)
+
+    def test_controlled_t_11(self):
+        steps = [[{"type": "X", "targets": [0, 1]}], [{"type": "T", "targets": [1], "controls": [0]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[1]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 0, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+        assert_complex_approx(amplitudes[2], 0, 0)
+        assert_complex_approx(amplitudes[3], sqrt(2) / 2, sqrt(2) / 2)
+
+    def test_controlled_t_2_controls_000(self):
+        steps = [[{"type": "T", "targets": [2], "controls": [0, 1]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[0]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 1, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+        assert_complex_approx(amplitudes[2], 0, 0)
+        assert_complex_approx(amplitudes[3], 0, 0)
+        assert_complex_approx(amplitudes[4], 0, 0)
+        assert_complex_approx(amplitudes[5], 0, 0)
+        assert_complex_approx(amplitudes[6], 0, 0)
+        assert_complex_approx(amplitudes[7], 0, 0)
+
+    def test_controlled_t_2_controls_011(self):
+        steps = [[{"type": "X", "targets": [0, 1]}], [{"type": "T", "targets": [2], "controls": [0, 1]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[1]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 0, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+        assert_complex_approx(amplitudes[2], 0, 0)
+        assert_complex_approx(amplitudes[3], 1, 0)
+        assert_complex_approx(amplitudes[4], 0, 0)
+        assert_complex_approx(amplitudes[5], 0, 0)
+        assert_complex_approx(amplitudes[6], 0, 0)
+        assert_complex_approx(amplitudes[7], 0, 0)
+
+    def test_controlled_t_1_control_2_targets(self):
+        steps = [[{"type": "T", "targets": [1, 2], "controls": [0]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[0]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 1, 0)
+        assert_complex_approx(amplitudes[1], 0, 0)
+        assert_complex_approx(amplitudes[2], 0, 0)
+        assert_complex_approx(amplitudes[3], 0, 0)
+        assert_complex_approx(amplitudes[4], 0, 0)
+        assert_complex_approx(amplitudes[5], 0, 0)
+        assert_complex_approx(amplitudes[6], 0, 0)
+        assert_complex_approx(amplitudes[7], 0, 0)
+
+    def test_controlled_t_1_control_2_targets_011(self):
+        steps = [[{"type": "X", "targets": [0]}], [{"type": "T", "targets": [1, 2], "controls": [0]}]]
+
+        result = self.qiskit_runner.run_circuit(steps)
+
+        amplitudes = result[1]["amplitudes"]
+        assert_complex_approx(amplitudes[0], 0, 0)
+        assert_complex_approx(amplitudes[1], 1, 0)
+        assert_complex_approx(amplitudes[2], 0, 0)
+        assert_complex_approx(amplitudes[3], 0, 0)
+        assert_complex_approx(amplitudes[4], 0, 0)
+        assert_complex_approx(amplitudes[5], 0, 0)
+        assert_complex_approx(amplitudes[6], 0, 0)
+        assert_complex_approx(amplitudes[7], 0, 0)
