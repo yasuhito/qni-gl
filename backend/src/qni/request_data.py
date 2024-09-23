86418a5cd7ced852e50e9e7f0b35073fb824d228
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-23 11:46:38 +0900

    Update request_data.py

--- backend/src/qni/request_data.py
+++ backend/src/qni/request_data.py
@@ -4,23 +4,23 @@ import json
 
 from qni.types import DeviceType
 
-DEFAULT_QUBIT_COUNT: int | None = None
-DEFAULT_UNTIL_STEP_INDEX: int | None = None
-
 
 class RequestData:
+    DEFAULT_QUBIT_COUNT = 0
+    DEFAULT_UNTIL_STEP_INDEX = 0
+
     def __init__(self, form: dict | None = None):
         form = form or {}
         self.circuit_id: str = form.get("id", "")
-        self.qubit_count: int = self._get_int_from_request(form, "qubitCount", DEFAULT_QUBIT_COUNT)
-        self.until_step_index: int = self._get_int_from_request(form, "untilStepIndex", DEFAULT_UNTIL_STEP_INDEX)
+        self.qubit_count: int = self._get_int_from_request(form, "qubitCount", self.DEFAULT_QUBIT_COUNT)
+        self.until_step_index: int = self._get_int_from_request(form, "untilStepIndex", self.DEFAULT_UNTIL_STEP_INDEX)
         self.steps: list[dict] = self._get_steps_from_request(form)
         self.amplitude_indices: list[int] = self._get_amplitude_indices_from_request(form)
-        self.device: DeviceType = DeviceType.GPU if form.get("useGpu", "false").lower() == "true" else DeviceType.CPU
+        self.device: DeviceType = self._get_device_type(form)
 
     @staticmethod
-    def _get_int_from_request(form: dict, key: str, default: int | None = None) -> int:
-        return int(form.get(key, default or 0))
+    def _get_int_from_request(form: dict, key: str, default: int) -> int:
+        return int(form.get(key, default) or default)
 
     @staticmethod
     def _get_amplitude_indices_from_request(form: dict) -> list[int]:
@@ -29,3 +29,7 @@ class RequestData:
     @staticmethod
     def _get_steps_from_request(form: dict) -> list[dict]:
         return json.loads(form.get("steps", "[]"))
+
+    @staticmethod
+    def _get_device_type(form: dict) -> DeviceType:
+        return DeviceType.GPU if form.get("useGpu", "false").lower() == "true" else DeviceType.CPU
