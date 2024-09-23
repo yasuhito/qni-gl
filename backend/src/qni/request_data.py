c651e2f969cb82eaeafc40127b1a673175757bf2
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-23 13:10:59 +0900

    Update request_data.py

--- backend/src/qni/request_data.py
+++ backend/src/qni/request_data.py
@@ -9,23 +9,20 @@ if TYPE_CHECKING:
 from qni.types import DeviceType
 
 
-def steps_type(parameter: str) -> list[dict]:
-    return json.loads(parameter)
-
-
-def amplitude_indices_type(parameter: str) -> list[int]:
-    return [int(each) for each in parameter.split(",") if each.isdigit()]
-
-
-def device_type(parameter: str) -> DeviceType:
-    return DeviceType(parameter)
-
-
 class RequestData:
     def __init__(self, form: ImmutableMultiDict[str, str]):
         self.circuit_id: str = form.get("id", "")
         self.qubit_count: int = form.get("qubitCount", 0, type=int)
         self.until_step_index: int = form.get("untilStepIndex", 0, type=int)
-        self.steps: list[dict] = form.get("steps", [], type=steps_type)
-        self.amplitude_indices: list[int] = form.get("amplitudeIndices", [], type=amplitude_indices_type)
-        self.device: DeviceType = form.get("device", DeviceType.CPU, type=device_type)
+        self.steps: list[dict] = form.get("steps", [], type=self._steps_type)
+        self.amplitude_indices: list[int] = form.get("amplitudeIndices", [], type=self._amplitude_indices_type)
+        self.device: DeviceType = form.get("device", DeviceType.CPU, type=self._device_type)
+
+    def _steps_type(self, parameter: str) -> list[dict]:
+        return json.loads(parameter)
+
+    def _amplitude_indices_type(self, parameter: str) -> list[int]:
+        return [int(each) for each in parameter.split(",") if each.isdigit()]
+
+    def _device_type(self, parameter: str) -> DeviceType:
+        return DeviceType(parameter)
