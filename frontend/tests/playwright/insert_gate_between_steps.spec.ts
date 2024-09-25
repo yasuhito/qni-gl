664e0619fdc0b004d2cecbb00efeaf0c254bb44a
Yasuhito Takamiya <yasuhito@hey.com>
2024-09-25 14:17:40 +0900

    テストを追加

--- frontend/tests/playwright/insert_gate_between_steps.spec.ts
+++ frontend/tests/playwright/insert_gate_between_steps.spec.ts
@@ -14,6 +14,18 @@ test.describe("Insert gate between steps", () => {
     await expect(page).toHaveScreenshot("insert-h-gate-before-step0.png");
   });
 
+  test("Insert a gate before the gate on the first step", async ({
+    page,
+    circuitInfo,
+  }) => {
+    const hGate = circuitInfo.gatePalette.hGate;
+
+    await dragAndDrop(page, hGate, { step: 0, bit: 0 });
+    await dragAndDrop(page, hGate, { beforeStep: 0, bit: 0 });
+
+    await expect(page).toHaveScreenshot("insert-h-gate-before-step0-gate0.png");
+  });
+
   test("Insert a gate after the first step", async ({ page, circuitInfo }) => {
     const hGate = circuitInfo.gatePalette.hGate;
 
