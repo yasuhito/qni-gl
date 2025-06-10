97195c28115cb7bcb51ebe521d2c619ec5e1385f
hayatetete <kashiwabuchi.hayate@tis.co.jp>
2025-06-10 18:14:56 +0900

    share modalにcircuit titleを追加

--- frontend/src/share-modal.ts
+++ frontend/src/share-modal.ts
@@ -19,10 +19,25 @@ export class ShareModal {
     if (backgroundOverlay) {
       backgroundOverlay.addEventListener("click", this.close.bind(this));
     }
+
+    const titleInput = document.getElementById(
+      "circuit-title-input"
+    ) as HTMLInputElement | null;
+    if (titleInput) {
+      titleInput.addEventListener("input", () => {
+        document.title = titleInput.value || "Qni GL";
+      });
+    }
   }
 
   open(): void {
     this.modalElement.classList.remove("hidden");
+    const titleInput = document.getElementById(
+      "circuit-title-input"
+    ) as HTMLInputElement | null;
+    if (titleInput) {
+      titleInput.focus();
+    }
   }
 
   close(): void {
